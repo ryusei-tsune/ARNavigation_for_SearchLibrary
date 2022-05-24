c38e1dd1e442b496d2847067ed1a91bf48ff824e
ryusei-tsune <rt1226.tennis@gmail.com>
2022-05-24 12:22:27 +0900

    fix CreateEnvironmentMap.cs

--- Assets/Scripts/CreateEnvironmentMap.cs
+++ Assets/Scripts/CreateEnvironmentMap.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.XR.ARFoundation;
@@ -19,6 +20,7 @@ public class CreateEnvironmentMap : MonoBehaviour
 
     List<GameObject> particles = new List<GameObject>();
     [SerializeField] Vector3[] allPoints;
+    private List<int> selected = new List<int>(); // 必要ないかも
     private static Vector3 yValue;
     private static bool placing = false;
     private static bool landmark = false;
@@ -48,11 +50,13 @@ public class CreateEnvironmentMap : MonoBehaviour
     }
     private void Update()
     {
-        /*　Began:画面に指が触れたとき
+        /*　TouchPhase
+        Began:画面に指が触れたとき
         Moved:画面上で指が動いたとき
         Stationary:指が画面に触れているが動いてはいないとき，
         Ended:画面から指が離れたとき
-        Canceled:システムがタッチの追跡をキャンセルしました*/
+        Canceled:システムがタッチの追跡をキャンセルしました
+        */
         if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
         {
             if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
@@ -73,11 +77,11 @@ public class CreateEnvironmentMap : MonoBehaviour
                     {
                         touchPosition.y = yValue.y;
                     }
+                    // navMeshに登録されている点を配列で取得
+                    allPoints = ConvertAllPoints();
+                    int dragIndex = FindClosest(touch);
                     if (placing)
                     {
-                        // navMeshに登録されている点を配列で取得
-                        allPoints = ConvertAllPoints();
-                        int dragIndex = FindClosest(touch);
                         particles.Add(Instantiate(particle));
                         particles[particles.Count - 1].transform.position = touchPosition;
 
@@ -199,5 +203,171 @@ public class CreateEnvironmentMap : MonoBehaviour
         {
             mRenderer.enabled = false;
         }
+    }
+    public void mapButton()
+    {
+        landmark = false;
+        // PTタグを付与した選択地点のパーティクルを検出
+        if (!placing)
+        {
+            GameObject newObj = newNavMesh.GetComponent<NavMeshObject>().CreateSubMesh();
+            placing = true;
+            statusText.text = "map";
+        }
+        else
+        {
+            confirmMap();
+        }
+    }
+
+    public void landMarkButton()
+    {
+        placing = false;
+        confirmMap();
+        if (!landmark)
+        {
+            landmark = true;
+        }
+        else
+        {
+            landmark = false;
+        }
+    }
+    // 指定した歩行可能領域の確定
+    private void confirmMap()
+    {
+        GameObject[] particleList = GameObject.FindGameObjectsWithTag("PT");
+        foreach (var item in particleList)
+        {
+            Destroy(item); //  item.SetActive(false);
+        }
+        CheckCombine();
+
+        // //get all mesh filters
+        MeshFilter[] meshFilters = navMesh.GetComponentsInChildren<MeshFilter>();
+        //let the navmesh combine them   
+        navMesh.Combine();
+        for (int i = 1; i < meshFilters.Length; i++)
+        {
+            Destroy(meshFilters[i].gameObject);
+        }
+    }
+
+    private void CheckCombine()
+    {
+
+        //get count of all submeshes
+        int subPointsCount = navMesh.subPoints.Count;
+        if (navMesh.subPoints.Count == 0) return;
+        NavMeshObject.SubPoints lastPoints = navMesh.subPoints[subPointsCount - 1];
+
+        if (lastPoints.list.Count <= 2)
+        {
+            selected.Clear();
+            for (int i = 0; i < lastPoints.list.Count; i++)
+            {
+                selected.Add(lastPoints.list[i]);
+            }
+
+            DeleteSelected();
+        }
+    }
+
+    //delete previously selected vertices and rebuild mesh
+    private void DeleteSelected(bool auto = false)
+    {
+        //get mesh references
+        MeshFilter filter = navMesh.GetComponent<MeshFilter>();
+        List<Vector3> vertices = null;
+        if (filter.sharedMesh != null)
+            vertices = new List<Vector3>(filter.sharedMesh.vertices);
+        //filter selected list for unique entries
+        selected = selected.Distinct().ToList();
+        selected = selected.OrderByDescending(x => x).ToList();
+
+        //loop over selected vertex indices
+        for (int i = 0; i < selected.Count; i++)
+        {
+            //remove index from mesh vertices
+            int index = selected[i];
+            if (vertices != null)
+            {
+                if (vertices.Contains(allPoints[index])) vertices.Remove(allPoints[index]);
+                else try { vertices.RemoveAt(index); } catch { };
+            }
+            navMesh.list.RemoveAt(index);
+
+            //loop over submeshes and remove it there too
+            for (int j = 0; j < navMesh.subPoints.Count; j++)
+            {
+                navMesh.subPoints[j].list.Remove(index);
+                //decrease higher entries, as the array is smaller now
+                for (int k = 0; k < navMesh.subPoints[j].list.Count; k++)
+                {
+                    if (navMesh.subPoints[j].list[k] >= index)
+                        navMesh.subPoints[j].list[k] -= 1;
+                }
+            }
+        }
+
+        //clear selection
+        selected.Clear();
+
+        //loop over submeshes to remove obsolete indices,
+        //e.g. if a submesh has only 2 vertices after removal
+        for (int i = navMesh.subPoints.Count - 1; i >= 0; i--)
+        {
+            //check for vertex count
+            if (navMesh.subPoints[i].list.Count <= 2)
+            {
+                //construct a combined list with all indices
+                List<int> allIndices = new List<int>();
+                for (int j = 0; j < navMesh.subPoints.Count; j++)
+                    allIndices.AddRange(navMesh.subPoints[j].list);
+
+                //check whether an index occurs more than once
+                List<int> duplicates = allIndices.GroupBy(x => x)
+                                       .Where(x => x.Count() > 1)
+                                       .Select(x => x.Key)
+                                       .ToList();
+
+                //if an index in this submesh is not being used in other
+                //submeshes anymore, this means that we can remove it too
+                for (int j = 0; j < navMesh.subPoints[i].list.Count; j++)
+                    if (!duplicates.Contains(navMesh.subPoints[i].list[j]))
+                        selected.Add(navMesh.subPoints[i].list[j]);
+
+                //delete this submesh entry
+                navMesh.subPoints.RemoveAt(i);
+            }
+        }
+
+        //recalculate triangles for complete mesh
+        if (filter.sharedMesh == null) return;
+        List<int> triangles = new List<int>();
+        for (int i = 0; i < navMesh.subPoints.Count; i++)
+            triangles.AddRange(navMesh.RecalculateTriangles(navMesh.subPoints[i].list));
+
+        //assign triangles and update vertices
+        filter.sharedMesh.triangles = triangles.ToArray();
+        navMesh.list = vertices;
+        navMesh.UpdateMesh(ConvertAllPoints());
+
+        //recursively delete the remaining obsolete indices
+        //which were found by looking through all submeshes
+        if (selected.Count > 0)
+        {
+            DeleteSelected();
+            return;
+        }
+
+        //deletion done - optimize mesh
+        OptimizeMesh(filter.sharedMesh);
+    }
+    //rebuild mesh properties
+    private void OptimizeMesh(Mesh Opmesh)
+    {
+        Opmesh.RecalculateNormals();
+        Opmesh.RecalculateBounds();
     }
 }
