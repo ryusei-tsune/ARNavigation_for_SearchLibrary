02c808c91b5ff36bc23b5bba9a154e95635ac354
ryusei-tsune <rt1226.tennis@gmail.com>
2022-05-25 16:17:46 +0900

    マーカ画像の検出

---
+++ Assets/Scripts/DetectARMarker.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARFoundation;
+
+
+/// <summary>
+/// ARマーカの座標を検出及び保存(後々座標変換を行う際に使用)
+/// </summary>
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class DetectARMarker : MonoBehaviour
+{
+    /// <summary>
+    /// The prefab has a world space UI canvas,
+    /// which requires a camera to function properly.
+    /// </summary>
+    [SerializeField] Camera m_WorldSpaceCanvasCamera;
+
+    /// <summary>
+    /// If an image is detected but no source texture can be found,
+    /// this texture is used instead.
+    /// </summary>
+    [SerializeField] Texture2D m_DefaultTexture;
+
+    private string filename;                //マーカの座標を保存
+
+    [SerializeField] Text statusText;              //状態をテキストで表示
+
+    ARTrackedImageManager m_TrackedImageManager;        //画像追跡を行うクラス
+    List<Vector3> markerPosList;
+    bool marker = false;
+
+
+    void Awake()
+    {
+        markerPosList = new List<Vector3>();
+        statusText.enabled = false;
+        m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+
+    void OnEnable()
+    {
+        m_TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    void UpdateInfo(ARTrackedImage trackedImage)
+    {
+        statusText.enabled = true;
+        //画像を認識した際に実行
+        if ((trackedImage.trackingState != TrackingState.None))
+        {
+            if (!marker)
+            {
+                statusText.text = "Recognize Image";
+                Vector3 pos = trackedImage.transform.position;
+                Vector3 markerPos = trackedImage.transform.TransformPoint(pos.x, pos.y, pos.z);
+                //認識した画像の名前を文字列に
+                markerPosList.Add(markerPos);
+                statusText.text = "x: " + markerPos.x + "\ny: " + markerPos.y + "\nz: " + markerPos.z;
+                filename = trackedImage.referenceImage.name;
+                //VariableCP.currentfloor = int.Parse(Regex.Replace(filename, @"[^0-9]", "")); //環境マップを読み込んだ階が何階か判定
+                //FileController.GetComponent<UFileIO>().enabled = true;  //UFileIO.csのプログラム起動
+            }
+        }
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+
+        foreach (var trackedImage in eventArgs.added)
+        {
+            // Give the initial image a reasonable default scale
+            trackedImage.transform.localScale = new Vector3(0.01f, 1f, 0.01f);
+
+            UpdateInfo(trackedImage);
+        }
+
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            UpdateInfo(trackedImage);
+        }
+
+    }
+}
