c3f775e0a7147fe94c644e556a2cf7adb247c5a3
ryusei-tsune <rt1226.tennis@gmail.com>
2022-05-27 16:57:31 +0900

    マーカ座標検出プログラム

--- Assets/Scripts/DetectARMarker.cs
+++ Assets/Scripts/DetectARMarker.cs
@@ -1,5 +1,9 @@
+using System;
+using System.IO;
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARSubsystems;
@@ -22,20 +26,19 @@ public class DetectARMarker : MonoBehaviour
     /// If an image is detected but no source texture can be found,
     /// this texture is used instead.
     /// </summary>
-    [SerializeField] Texture2D m_DefaultTexture;
 
-    private string filename;                //マーカの座標を保存
-
+    [SerializeField] InputField inputField;
     [SerializeField] Text statusText;              //状態をテキストで表示
 
     ARTrackedImageManager m_TrackedImageManager;        //画像追跡を行うクラス
-    List<Vector3> markerPosList;
-    bool marker = false;
-
+    Dictionary<string, Vector3> markerPosList;
+    bool[] marker = { false, false };
 
     void Awake()
     {
-        markerPosList = new List<Vector3>();
+        // marker = new bool[2];
+        // marker = Enumerable.Repeat<bool>(false, 2).ToArray();
+        markerPosList = new Dictionary<string, Vector3>();
         statusText.enabled = false;
         m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
     }
@@ -57,17 +60,28 @@ public class DetectARMarker : MonoBehaviour
         //画像を認識した際に実行
         if ((trackedImage.trackingState != TrackingState.None))
         {
-            if (!marker)
+            string markerName = trackedImage.referenceImage.name;
+            int num = int.Parse(Regex.Replace(markerName, @"[^0-9]", ""));
+
+            if (!marker[num])
             {
                 statusText.text = "Recognize Image";
                 Vector3 pos = trackedImage.transform.position;
                 Vector3 markerPos = trackedImage.transform.TransformPoint(pos.x, pos.y, pos.z);
                 //認識した画像の名前を文字列に
-                markerPosList.Add(markerPos);
-                statusText.text = "x: " + markerPos.x + "\ny: " + markerPos.y + "\nz: " + markerPos.z;
-                filename = trackedImage.referenceImage.name;
+                try
+                {
+                    markerPosList.Add(num.ToString(), markerPos);
+                    statusText.text = "x: " + markerPos.x + "\ny: " + markerPos.y + "\nz: " + markerPos.z;
+                }
+                catch (Exception e)
+                {
+                    statusText.text = "already" + e;
+                }
+
                 //VariableCP.currentfloor = int.Parse(Regex.Replace(filename, @"[^0-9]", "")); //環境マップを読み込んだ階が何階か判定
                 //FileController.GetComponent<UFileIO>().enabled = true;  //UFileIO.csのプログラム起動
+                marker[num] = true;
             }
         }
     }
@@ -88,5 +102,31 @@ public class DetectARMarker : MonoBehaviour
             UpdateInfo(trackedImage);
         }
 
+    }
+
+    public void PositionSave()
+    {
+        if (inputField.text != "")
+        {
+            statusText.text = "during PositionSave";
+            string filename = Application.persistentDataPath + "/" + inputField.text + "-MarkerPos.txt";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filename, true, Encoding.GetEncoding("utf-8")))
+                {
+                    foreach (var marker in markerPosList)
+                    {
+                        statusText.text = "json";
+                        string json = marker.Key + ": (" + marker.Value.x + ", " + marker.Value.y + ", " + marker.Value.z + ")";
+                        statusText.text = "filename";
+                        sw.WriteLine(json);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                statusText.text = e.ToString();
+            }
+        }
     }
 }
