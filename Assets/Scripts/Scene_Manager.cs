6e2c154fda2187818f8fc5a8ba08d261d15e8f27
ryusei-tsune <rt1226.tennis@gmail.com>
2022-07-16 19:34:31 +0900

    シーン変更時のスクリプトを追加

--- Assets/Scripts/Scene_Manager.cs
+++ Assets/Scripts/Scene_Manager.cs
@@ -6,12 +6,31 @@ public class Scene_Manager : MonoBehaviour
     // Start is called before the first frame update
     private int Sceneflag = 0;
 
-    public void changeScene()
+    public void OnClickAdmin()
     {
         if (Sceneflag == 0)
         {
-            SceneManager.LoadScene("LEM_NaviScene", LoadSceneMode.Single);
             Sceneflag = 1;
+            SceneManager.LoadScene("CEM_DMScene", LoadSceneMode.Single);
         }
     }
+
+    public void OnClickUser()
+    {
+        if (Sceneflag == 0)
+        {
+            Sceneflag = 1;
+            SceneManager.LoadScene("LEM_NaviScene", LoadSceneMode.Single);
+        }
+    }
+
+    public void OnClickReturn()
+    {
+        if (Sceneflag == 1)
+        {
+            Sceneflag = 0;
+            SceneManager.LoadScene("TitleScene", LoadSceneMode.Single);
+        }
+    }
+
 }
