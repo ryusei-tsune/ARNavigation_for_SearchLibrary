50c6d4cacc1af94108869375105740ea5e0e2da0
ryusei-tsune <rt1226.tennis@gmail.com>
2022-07-19 12:56:28 +0900

    シーン変更の処理を追加

--- Assets/Scripts/Scene_Manager.cs
+++ Assets/Scripts/Scene_Manager.cs
@@ -3,34 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class Scene_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private int Sceneflag = 0;
 
     public void OnClickAdmin()
     {
-        if (Sceneflag == 0)
-        {
-            Sceneflag = 1;
-            SceneManager.LoadScene("CEM_DMScene", LoadSceneMode.Single);
-        }
+        SceneManager.LoadScene("CEM_DMScene", LoadSceneMode.Single);
+
     }
 
     public void OnClickUser()
     {
-        if (Sceneflag == 0)
-        {
-            Sceneflag = 1;
-            SceneManager.LoadScene("LEM_NaviScene", LoadSceneMode.Single);
-        }
+        SceneManager.LoadScene("LEM_NaviScene", LoadSceneMode.Single);
+
     }
 
     public void OnClickReturn()
     {
-        if (Sceneflag == 1)
-        {
-            Sceneflag = 0;
-            SceneManager.LoadScene("TitleScene", LoadSceneMode.Single);
-        }
+        SceneManager.LoadScene("TitleScene", LoadSceneMode.Single);
     }
 
 }
