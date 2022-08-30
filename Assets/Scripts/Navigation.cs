93c5224fb87126e19ede1719eeaa7a98e25b4fd4
ryusei-tsune <rt1226.tennis@gmail.com>
2022-08-30 11:46:55 +0900

    ナビゲーションプログラムの修正

--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -1,66 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AI;
 
 public class Navigation : MonoBehaviour
 {
-    [SerializeField] Text statusText;
-    [SerializeField] InputField searchedKey;
-    private LineRenderer line = null;
+    // private LineRenderer line = null;
     private GameObject agent;
     private GameObject[] maps;
     private NavMeshAgent navAgent;
     private NavMeshPath path;
 
-    private void Start()
-    {
-        agent = GameObject.FindWithTag("MainCamera");
-        line = agent.GetComponent<LineRenderer>();
-        line.enabled = false;
-    }
     public void NavigationButton()
     {
-        if (CommonVariables.destinationList.Count > 0)
-        {
-            maps = GameObject.FindGameObjectsWithTag("MapGameObject");
-            foreach (GameObject map in maps)
-            {
-                map.GetComponent<NavMeshSurface>().BuildNavMesh();
-            }
-            agent.AddComponent<NavMeshAgent>();
-            line.enabled = true;
-            navAgent = agent.GetComponent<NavMeshAgent>();
+        ExtractKeyword();
+        GameObject ScrollView = GameObject.FindGameObjectWithTag("ScrollView");
+        ScrollView.SetActive(false);
+        // agent = GameObject.FindWithTag("MainCamera");
+        // line = agent.GetComponent<LineRenderer>();
+        
+        // if (CommonVariables.destinationList.Count > 0)
+        // {
+        //     maps = GameObject.FindGameObjectsWithTag("MapGameObject");
+        //     foreach (GameObject map in maps)
+        //     {
+        //         map.GetComponent<NavMeshSurface>().BuildNavMesh();
+        //     }
+        //     agent.AddComponent<NavMeshAgent>();
+        //     line.enabled = true;
+        //     navAgent = agent.GetComponent<NavMeshAgent>();
             
-            GameObject dest = null;
-            string keyword = searchedKey.text;
-            foreach(GameObject target in CommonVariables.destinationList){
-                if (target.GetComponent<TextMesh>().text.Contains(keyword))
-                {
-                    dest = target;
-                }
-            }
-            navAgent.radius = 0.1f;
-            if (dest != null)
-            {
-                navAgent.SetDestination(dest.transform.position);
-                navAgent.speed = 0.0f;
+        //     GameObject dest = null;
+        //     string keyword = searchedKey.text;
+        //     foreach(GameObject target in CommonVariables.destinationList){
+        //         if (target.GetComponent<TextMesh>().text.Contains(keyword))
+        //         {
+        //             dest = target;
+        //         }
+        //     }
+        //     navAgent.radius = 0.1f;
+        //     if (dest != null)
+        //     {
+        //         navAgent.SetDestination(dest.transform.position);
+        //         navAgent.speed = 0.0f;
 
-                path = new NavMeshPath();
-                navAgent.CalculatePath(dest.transform.position, path);
+        //         path = new NavMeshPath();
+        //         navAgent.CalculatePath(dest.transform.position, path);
 
-                line.positionCount = path.corners.Length;
-                line.SetPositions(path.corners);
+        //         line.positionCount = path.corners.Length;
+        //         line.SetPositions(path.corners);
 
-                statusText.text = "キーワード: " + keyword;
-            }
-            else
-            {
-                statusText.text = "キーワードと一致する本はありません！";
-            }
-        }
-        else
-        {
-            statusText.text = "本が登録されていません！";
+        //         statusText.text = "キーワード: " + keyword;
+        //     }
+        //     else
+        //     {
+        //         statusText.text = "キーワードと一致する本はありません！";
+        //     }
+        // }
+        // else
+        // {
+        //     statusText.text = "本が登録されていません！";
+        // }
+    }
+
+    private void ExtractKeyword()
+    {
+        BookInformation.bookTitle = this.transform.GetChild(0).GetComponent<Text>().text;
+        BookInformation.bookAuthor = this.transform.GetChild(1).GetComponent<Text>().text;
+        
+        string bookPosition = this.transform.GetChild(2).GetComponent<Text>().text;
+        int index = Regex.Matches(bookPosition, @"\d")[0].Index;
+        BookInformation.floor = int.Parse(bookPosition[index].ToString());
+        
+        string code = bookPosition.Substring(index+3);
+        BookInformation.bookCode = Regex.Replace(code, @"[^0-9a-zA-Z/.]+", ""); //本棚のIDを取得
+
+        if (BookSearch.instance) {
+            BookSearch.instance.ChangeText();
         }
     }
 }
